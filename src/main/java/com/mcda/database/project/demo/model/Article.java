9eda3f0a292f301cdd7dbcfac697c975ff90e9e2
Allianzcortex <iamwanghz@gmail.com>
2019-04-08 11:02:59 -0300

    add bootstrap and config some hard-coded variable

--- src/main/java/com/mcda/database/project/demo/model/Article.java
+++ src/main/java/com/mcda/database/project/demo/model/Article.java
@@ -16,7 +16,7 @@ import java.util.Set;
 public class Article {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "Article_ID")
     private int id;
 
--- src/main/java/com/mcda/database/project/demo/repository/AllRepository.java
+++ src/main/java/com/mcda/database/project/demo/repository/AllRepository.java
@@ -1,20 +1,15 @@
 package com.mcda.database.project.demo.repository;
 
 import com.zaxxer.hikari.util.DriverDataSource;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.Repository;
-import org.springframework.data.repository.query.Param;
+import org.springframework.beans.factory.annotation.Value;
+
+
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-import javax.sql.DataSource;
 import javax.transaction.Transactional;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -26,21 +21,29 @@ public class AllRepository {
 
     private JdbcTemplate jdbcTemplate;
 
-    public AllRepository() {
+    @Value("${spring.database.name}")
+    private String databaseName;
+
+
+    public AllRepository(@Value("${spring.datasource.url}") String databaseUrl
+            , @Value("${spring.datasource.username}") String username,
+                         @Value("${spring.datasource.password}") String password) {
 
         DriverManagerDataSource ds = new DriverManagerDataSource();
         ds.setDriverClassName("com.mysql.jdbc.Driver");
-        System.out.println("=-=--------------------------");
-        ds.setUrl("jdbc:mysql://localhost:3306/database_project");
-        ds.setUsername("root");
-        ds.setPassword("root");
+        System.out.println("url is " + databaseUrl);
+        ds.setUrl(databaseUrl);
+        ds.setUsername(username);
+        ds.setPassword(password);
         this.jdbcTemplate = new JdbcTemplate(ds);
     }
 
     @Transactional
-    public List<String> findAllAvailableTable(){
+    public List<String> findAllAvailableTable() {
         // Just hard-coded the database name
-        String query = "SELECT table_name FROM information_schema.tables WHERE table_schema ='database_project';";
+
+
+        String query = "SELECT table_name FROM information_schema.tables WHERE table_schema ='" + databaseName + "';";
         return this.jdbcTemplate.query(query, new RowMapper<String>() {
             public String mapRow(ResultSet rs, int rowNum)
                     throws SQLException {
@@ -56,12 +59,11 @@ public class AllRepository {
     }
 
 
-
     @Transactional
     public List<String> findAllTableFields(String tableName) {
         String query = "select COLUMN_NAME " +
                 "  from information_schema.columns " +
-                " where table_schema ='" + "database_project" +
+                " where table_schema ='" + databaseName +
                 "'   and table_name = '" + tableName + "'";
 
         return this.jdbcTemplate.query(query, new RowMapper<String>() {
